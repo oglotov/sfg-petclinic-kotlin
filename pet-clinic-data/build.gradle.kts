10e09c1343c27be2c6c51f972d96e7561a8498fb
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-11-27 02:31:49 +0200

    Refactoring to multimodule gradle project. Closed #1

--- pet-clinic-data/build.gradle.kts
+++ pet-clinic-data/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0" apply false
+    id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
@@ -32,25 +32,20 @@ dependencyManagement {
 dependencies {
 
     // Kotlin
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // Spring Starter
-    implementation("org.springframework.boot:spring-boot-starter")
 
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    // Databases
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -61,9 +56,5 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
