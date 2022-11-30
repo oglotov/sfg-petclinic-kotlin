877690eb3cd9deea0579266b3c64b240be1764fe
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-11-30 19:32:20 +0200

    Added Owners list and update controller. Closed #22

--- pet-clinic-web/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/controllers/OwnerController.kt
+++ pet-clinic-web/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/controllers/OwnerController.kt
@@ -1,12 +1,19 @@
 package ua.wwind.glotov.sfgpetclinickotlin.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import ua.wwind.glotov.sfgpetclinickotlin.services.OwnerService
 
 @RequestMapping("/owners")
 @Controller
-class OwnerController {
+class OwnerController @Autowired constructor(private val ownerService: OwnerService) {
+
     @GetMapping("", "/", "/index", "/index.html")
-    fun listOwners() = "owners/index"
+    fun listOwners(model: Model): String {
+        model.addAttribute("owners", ownerService.findAll())
+        return "owners/index"
+    }
 }
