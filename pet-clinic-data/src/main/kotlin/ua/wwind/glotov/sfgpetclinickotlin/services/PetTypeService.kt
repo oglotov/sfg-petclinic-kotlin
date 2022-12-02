cb766967c785bc3f3d0526568500027366ad3690
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-12-03 00:57:02 +0200

    Created PetType map service. Closed #28

---
+++ pet-clinic-data/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/services/PetTypeService.kt
@@ -0,0 +1,5 @@
+package ua.wwind.glotov.sfgpetclinickotlin.services
+
+import ua.wwind.glotov.sfgpetclinickotlin.model.PetType
+
+interface PetTypeService: CrudService<PetType, Long>
---
+++ pet-clinic-data/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/services/map/PetTypeMapService.kt
@@ -0,0 +1,6 @@
+package ua.wwind.glotov.sfgpetclinickotlin.services.map
+
+import ua.wwind.glotov.sfgpetclinickotlin.model.PetType
+import ua.wwind.glotov.sfgpetclinickotlin.services.PetTypeService
+
+class PetTypeMapService: AbstractMapService<PetType>(), PetTypeService
