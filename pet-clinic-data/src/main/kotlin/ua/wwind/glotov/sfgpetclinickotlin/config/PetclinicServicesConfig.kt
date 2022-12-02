484436041912bcaf0cf2816cb893d17ad902245e
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-12-03 00:59:24 +0200

    Added PetType service to Spring config. Closed #28

--- pet-clinic-data/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/config/PetclinicServicesConfig.kt
+++ pet-clinic-data/src/main/kotlin/ua/wwind/glotov/sfgpetclinickotlin/config/PetclinicServicesConfig.kt
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ua.wwind.glotov.sfgpetclinickotlin.services.OwnerService
 import ua.wwind.glotov.sfgpetclinickotlin.services.PetService
+import ua.wwind.glotov.sfgpetclinickotlin.services.PetTypeService
 import ua.wwind.glotov.sfgpetclinickotlin.services.VetSevice
 import ua.wwind.glotov.sfgpetclinickotlin.services.map.OwnerMapService
 import ua.wwind.glotov.sfgpetclinickotlin.services.map.PetMapService
+import ua.wwind.glotov.sfgpetclinickotlin.services.map.PetTypeMapService
 import ua.wwind.glotov.sfgpetclinickotlin.services.map.VetMapService
 
 @Configuration
@@ -19,4 +21,7 @@ class PetclinicServicesConfig {
 
     @Bean
     fun vetService(): VetSevice = VetMapService()
+
+    @Bean
+    fun petTypeService(): PetTypeService = PetTypeMapService()
 }
